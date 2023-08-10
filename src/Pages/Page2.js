4ca350fa7361f5c49240b973ffd669acfe6d4454
Gucci Gangnam <diviner02hive@icloud.com>
2023-08-10 16:02:37 +0700

    Page 2 started. First column complete. NEXT - Finish page 2

--- src/Pages/Page2.js
+++ src/Pages/Page2.js
@@ -5,16 +5,35 @@ export const Page2 = () => {
     return (
         <div className='Page2'>
 
-            <div className='Brand1'> 
-            Brand 1
+            <div className='BrandPreviewDiv'> 
+
+            <div className='BrandAOverlay'>
+            Brand Name A
             </div>
-            <div className='Brand2'> 
+
+            <img src='./Images/BrandA1.jpg' alt='Brand Image'/>
+            <img src='./Images/BrandA2.jpg' alt='Brand Image'/>
+            <img src='./Images/BrandA3.jpg' alt='Brand Image'/>
+            <img src='./Images/BrandA5.jpg' alt='Brand Image'/>
+            <img src='./Images/BrandA4.jpg' alt='Brand Image'/>
+            <img src='./Images/BrandA6.jpg' alt='Brand Image'/>
+            <img src='./Images/BrandA1.jpg' alt='Brand Image'/>
+            <img src='./Images/BrandA2.jpg' alt='Brand Image'/>
+            <img src='./Images/BrandA3.jpg' alt='Brand Image'/>
+            <img src='./Images/BrandA5.jpg' alt='Brand Image'/>
+            <img src='./Images/BrandA4.jpg' alt='Brand Image'/>
+            <img src='./Images/BrandA6.jpg' alt='Brand Image'/>
+            </div>
+
+            <div className='BrandPreviewDiv'> 
             Bramd 2
             </div>
-            <div className='Brand3'> 
+            
+            <div className='BrandPreviewDiv'> 
             Bramd 3
             </div>
-            <div className='Brand4'> 
+
+            <div className='BrandPreviewDiv'> 
             Brand 4
             </div>
 
