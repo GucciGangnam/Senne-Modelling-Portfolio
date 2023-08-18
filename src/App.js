82059a36a3f87a1ef3e30ce6369c10f96c93102a
Gucci Gangnam <diviner02hive@icloud.com>
2023-08-18 13:11:46 +0700

    Page2.3 designed and built. Needs finishing. NEXT: add full picture fromfile pop up screen to each bran profile page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Page1 } from './Pages/Page1'
 import { Page2 } from './Pages/Page2'
 import { Page22 } from './Pages/Page22'
+import { Page23 } from './Pages/Page23'
 import { Page3 } from './Pages/Page3'
 import { Page4 } from './Pages/Page4'
 
@@ -14,8 +15,8 @@ function App() {
   return (
     <div className="App">
       <Page1/>
-      {/* <Page2/> */}
-      <Page22/>
+
+      <Page23/>
       <Page3/>
       <Page4/>
     </div>
---
+++ src/Pages/Page23.js
@@ -0,0 +1,64 @@
+// import styles 
+import './Page23.css'
+
+export const Page23 = () => { 
+    return ( 
+        <div className='Page23'> 
+
+            <div className='Slide'> 
+
+            <div className='SlideDots'>
+            • ◦ •
+            </div>
+
+                <div className='InnerSlide1'>
+                    <div className='PictureDiv'>
+                        <img className='BrandImage' src='./Images/BrandA2.jpg' alt='Brand Image'/>
+                    </div>
+                    <div className='BrandInfoDiv'>
+                        <div className='BrandName'>
+                            Brand
+                        </div>
+                    </div>
+                </div>
+
+                <div className='InnerSlide2'>
+                    <div className='PictureDiv'>
+                        <img className='BrandImage' src='./Images/BrandB8.jpeg' alt='Brand Image'/>
+                    </div>
+                    <div className='BrandInfoDiv'>
+                        <div className='BrandName'>
+                            Brand
+                        </div>
+                    </div>
+                </div>
+
+                <div className='InnerSlide3'>
+                    <div className='PictureDiv'>
+                        <img className='BrandImage' src='./Images/BrandC3.jpg' alt='Brand Image'/>
+                    </div>
+                    <div className='BrandInfoDiv'>
+                        <div className='BrandName'>
+                            Brand
+                        </div>
+                    </div>
+                </div>
+
+                <div className='InnerSlide4'>
+                    <div className='PictureDiv'>
+                        <img className='BrandImage' src='./Images/BrandA2.jpg' alt='Brand Image'/>
+                    </div>
+                    <div className='BrandInfoDiv'>
+                        <div className='BrandName'>
+                            Brand
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            
+
+
+        </div>
+    )
+}
