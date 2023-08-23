19f577efa34e1b7a5d234b6db5d2394b0fd5cb4d
Gucci Gangnam <diviner02hive@icloud.com>
2023-08-23 12:28:05 +0700

    All pages complete and styled. buttons and overlays working. NEXT: Compress and add all studio picture - THEN: Add bookmarks for the BIO and CONTCAT pages

--- src/Pages/Page23.js
+++ src/Pages/Page23.js
@@ -2,8 +2,121 @@
 import './Page23.css'
 
 export const Page23 = () => { 
+
+    //Dipslay collewction functions
+    function DisplayCollection1() { 
+        console.log("collection1 clicked");
+        let collection1 = document.querySelector('.Overlay1');
+        collection1.style.display = 'grid';
+
+        let dots = document.querySelector('.SlideDots')
+        dots.classList.add('open');
+        dots.innerHTML = 'X';
+
+
+        dots.addEventListener('click', function(){
+            collection1.style.display = 'none';
+            dots.classList.remove('open');
+            dots.innerHTML = '• ◦ •';
+        })
+    }
+    function DisplayCollection2() { 
+        console.log("collection2 clicked");
+        let collection2 = document.querySelector('.Overlay2');
+        collection2.style.display = 'grid';
+
+        let dots = document.querySelector('.SlideDots')
+        dots.classList.add('open');
+        dots.innerHTML = 'X';
+
+        dots.addEventListener('click', function(){
+            collection2.style.display = 'none';
+            dots.classList.remove('open');
+            dots.innerHTML = '• ◦ •';
+        })
+    }
+    function DisplayCollection3() { 
+        console.log("collection3 clicked");
+        let collection3 = document.querySelector('.Overlay3');
+        collection3.style.display = 'grid';
+
+        let dots = document.querySelector('.SlideDots')
+        dots.classList.add('open');
+        dots.innerHTML = 'X';
+
+        dots.addEventListener('click', function(){
+            collection3.style.display = 'none';
+            dots.classList.remove('open');
+            dots.innerHTML = '• ◦ •';
+        })
+    }
+    function DisplayCollection4() { 
+        console.log("collection4 clicked");
+        let collection4 = document.querySelector('.Overlay4');
+        collection4.style.display = 'grid';
+
+        let dots = document.querySelector('.SlideDots')
+        dots.classList.add('open');
+        dots.innerHTML = 'X';
+
+        dots.addEventListener('click', function(){
+            collection4.style.display = 'none';
+            dots.classList.remove('open');
+            dots.innerHTML = '• ◦ •';
+        })
+    }
+
+
+
     return ( 
         <div className='Page23'> 
+            <div className='Overlay1'> 
+            <img className='CollectionPhoto' src='./Images/BrandA2.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA3.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA6.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA2.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA3.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA6.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA2.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA3.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA6.jpg' alt='Brand Image'/>
+            </div>
+
+            <div className='Overlay2'> 
+            <img className='CollectionPhoto' src='./Images/BrandB3.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandB5.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandB8.jpeg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandB3.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandB5.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandB8.jpeg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandB3.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandB5.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandB8.jpeg' alt='Brand Image'/>
+            </div>
+
+            <div className='Overlay3'> 
+            <img className='CollectionPhoto' src='./Images/BrandC1.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandC2.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandC3.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandC1.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandC2.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandC3.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandC1.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandC2.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandC3.jpg' alt='Brand Image'/>
+            </div>
+
+            <div className='Overlay4'> 
+            <img className='CollectionPhoto' src='./Images/BrandA2.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA3.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA6.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA2.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA3.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA6.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA2.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA3.jpg' alt='Brand Image'/>
+            <img className='CollectionPhoto' src='./Images/BrandA6.jpg' alt='Brand Image'/>
+            </div>
 
             <div className='Slide'> 
 
@@ -16,11 +129,11 @@ export const Page23 = () => {
                         <img className='BrandImage' src='./Images/BrandA2.jpg' alt='Brand Image'/>
                     </div>
                     <div className='BrandInfoDiv'>
-                        <div className='BrandName'>
-                            Brand
+                        <div className='BrandName1'>
+                            SHIM STUDIO
                         </div>
                         <button className='CollectionButton'>
-                            <img className='CollectionImage' src='./Icons/Collection.png'/>
+                            <img className='CollectionImage' onClick={DisplayCollection1} src='./Icons/Collection.png'/>
                         </button>
                     </div>
                 </div>
@@ -30,11 +143,11 @@ export const Page23 = () => {
                         <img className='BrandImage' src='./Images/BrandB8.jpeg' alt='Brand Image'/>
                     </div>
                     <div className='BrandInfoDiv'>
-                        <div className='BrandName'>
-                            Brand
+                        <div className='BrandName2'>
+                            Ralph Lauren
                         </div>
                         <button className='CollectionButton'>
-                            <img className='CollectionImage' src='./Icons/Collection.png'/>
+                            <img className='CollectionImage' onClick={DisplayCollection2} src='./Icons/Collection.png'/>
                         </button>
                     </div>
                 </div>
@@ -44,11 +157,11 @@ export const Page23 = () => {
                         <img className='BrandImage' src='./Images/BrandC3.jpg' alt='Brand Image'/>
                     </div>
                     <div className='BrandInfoDiv'>
-                        <div className='BrandName'>
-                            Brand
+                        <div className='BrandName3'>
+                            THE SOUL
                         </div>
                         <button className='CollectionButton'>
-                            <img className='CollectionImage' src='./Icons/Collection.png'/>
+                            <img className='CollectionImage' onClick={DisplayCollection3} src='./Icons/Collection.png'/>
                         </button>
                     </div>
                 </div>
@@ -58,11 +171,11 @@ export const Page23 = () => {
                         <img className='BrandImage' src='./Images/BrandA2.jpg' alt='Brand Image'/>
                     </div>
                     <div className='BrandInfoDiv'>
-                        <div className='BrandName'>
+                        <div className='BrandName4'>
                             Brand
                         </div>
                         <button className='CollectionButton'>
-                            <img className='CollectionImage' src='./Icons/Collection.png'/>
+                            <img className='CollectionImage' onClick={DisplayCollection4} src='./Icons/Collection.png'/>
                         </button>
                         
                     </div>
